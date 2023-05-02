aec9ce6743e1fd43f49541a5a007cae3e3e97751
Aravindo Wingeier <wingeier@adobe.com>
2023-05-01 18:57:14 -0700

    browserAction

--- background.js
+++ background.js
@@ -3,29 +3,29 @@
 async function activateIcon(tab) {
     // Update Icon in toolbar
     let status = await getStatus(tab.url)
-    await chrome.action.setPopup({popup: "popup/popup.html", tabId: tab.id})
+    await browser.browserAction.setPopup({popup: "popup/popup.html", tabId: tab.id})
     await updateIcon(tab.id, status)
 }
 
 async function activateTabContent(tab) {
     // activate UI changes in content.js
-    await chrome.scripting.executeScript({
+    await browser.scripting.executeScript({
         target: {tabId: tab.id},
         files: ["global.js"],
     });
-    await chrome.scripting.executeScript({
+    await browser.scripting.executeScript({
         target: {tabId: tab.id},
         files: ["content/content.js"],
     });
-    await chrome.scripting.insertCSS({
+    await browser.scripting.insertCSS({
         target: {tabId: tab.id},
         files: ["content/content.css"],
     });
-    chrome.tabs.sendMessage(tab.id, {type: 'update-content'})
+    browser.tabs.sendMessage(tab.id, {type: 'update-content'})
 }
 
 
-chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
+browser.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
     // tab.url will be null if the permission is missing for this domain
     if (tab.url) {
         if (tab.status === "loading") {
@@ -38,12 +38,12 @@ chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
 })
 
 
-chrome.runtime.onMessage.addListener(async function (message, sender, sendResponse) {
+browser.runtime.onMessage.addListener(async function (message, sender, sendResponse) {
         // status changed in popup
         if (message.type === "set-status") {
             console.log("updating status to", message.status)
             await storePageStatus(message.tab.url, message.status);
-            chrome.tabs.sendMessage(message.tab.id, {type: "update-content"});
+            browser.tabs.sendMessage(message.tab.id, {type: "update-content"});
             await updateIcon(message.tab.id, message.status);
         }
         if (message.type === "access-granted") {
@@ -51,7 +51,7 @@ chrome.runtime.onMessage.addListener(async function (message, sender, sendRespon
         }
         if (message.type === "import-data") {
             for (let entry of message.data) {
-                chrome.storage.local.set({[entry.url]: entry.status});
+                browser.storage.local.set({[entry.url]: entry.status});
             }
             sendResponse("success")
 
@@ -61,18 +61,18 @@ chrome.runtime.onMessage.addListener(async function (message, sender, sendRespon
 
 
 async function updateIcon(tabId, status) {
-    await chrome.action.setIconP({tabId: tabId, path: `images/icon-${status}.png`});
+    await browser.browserAction.setIconP({tabId: tabId, path: `images/icon-${status}.png`});
 }
 
 async function storePageStatus(url, status) {
     let preparedUrl = prepareUrl(url);
 
     if (status === STATUS_NONE) {
-        return chrome.storage.local.remove(preparedUrl);
+        return browser.storage.local.remove(preparedUrl);
     } else {
         // this special syntax uses the value of preparedUrl as the key of the object
-        return chrome.storage.local.set({[preparedUrl]: status});
+        return browser.storage.local.set({[preparedUrl]: status});
     }
 }
 
-chrome.action.setIconP = promisify(chrome.action.setIcon, chrome.action)
+browser.browserAction.setIconP = promisify(browser.browserAction.setIcon, browser.browserAction)
