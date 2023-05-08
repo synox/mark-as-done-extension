9e80747683edcfbf7fbbebb0d66c71cf8c5a1c8d
Aravindo Wingeier <wingeier@adobe.com>
2023-05-07 20:35:00 -0700

    style list

--- src/list/list.js
+++ src/list/list.js
@@ -13,24 +13,34 @@ async function init() {
     h2.innerText = `${domain} (${doneCount}/${totalCount})`;
     summary.append(h2);
     details.append(summary);
-    const ul = document.createElement('ul');
+
+    const table = document.createElement('table');
     const items = linksByDomain[domain];
     sortLinksByStatus(items);
     // eslint-disable-next-line no-restricted-syntax
     for (const item of items) {
-      const li = document.createElement('li');
+      const row = document.createElement('tr');
+
+      const cellStatus = document.createElement('td');
+      cellStatus.innerText = item.status;
+      const icon = document.createElement('img');
+      icon.src = browser.runtime.getURL(`images/icon-${item.status}.png`);
+      cellStatus.prepend(icon);
+      row.append(cellStatus);
+
       const a = document.createElement('a');
       a.href = item.url;
       a.innerText = item.url;
       a.target = '_blank';
-      // a.classList.add(`marked-as--${item.status}`);
-      const icon = document.createElement('img');
-      icon.src = browser.runtime.getURL(`images/icon-${item.status}.png`);
-      a.prepend(icon);
-      li.append(a);
-      ul.append(li);
+
+      const cellLink = document.createElement('td');
+      cellLink.append(a);
+      row.append(cellLink);
+
+
+      table.append(row);
     }
-    details.append(ul);
+    details.append(table);
     listElement.append(details);
   }
 }
