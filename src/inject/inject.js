3536472387e91418021692c3d0104dddc08c3136
Aravindo Wingeier <wingeier@adobe.com>
2023-05-20 18:27:41 -0700

    simplify inject

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,43 +5,16 @@
 
 console.log('Content script injected');
 
-// TODO: move all storage access into the service worker
-
 // eslint-disable-next-line no-unused-vars
-browser.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.debug('content: received', request);
   if (request.type === 'update-content') {
     console.debug('content: updating content');
-    await updateAllLinksOnPage(window.location.href);
-
-    // Some pages load content later. Need to add a trigger to process the links later.
-    if (window.location.href.startsWith('https://learning.oreilly.com/')) {
-      // Button to show the toc
-      document.querySelectorAll('a.sbo-toc-thumb').forEach((a) => {
-        a.addEventListener('click', () => updateAllLinksOnPage(window.location.href));
-      });
-    }
-
-    if (window.location.href.startsWith('https://wiki.corp')) {
-      // eslint-disable-next-line no-unused-vars
-      const mutationObserver = new MutationObserver((mutationList, observer) => {
-        // Use traditional 'for loops' for IE 11
-        for (const mutation of mutationList) {
-          if (mutation.type === 'childList') {
-            console.debug('Wiki: Sidebar was loaded.');
-            updateAllLinksOnPage(window.location.href);
-          }
-        }
-      });
-      mutationObserver.observe(
-        document.querySelector('div.plugin_pagetree_children'),
-        { childList: true, subtree: false },
-      );
-    }
+    updateAllLinksOnPage(window.location.href).then(() => watchPageForDynamicallyAddedLinks());
   }
 });
 
-async function getStatusFromService(url) {
+async function getStatusFromServiceWorker(url) {
   return await browser.runtime.sendMessage({ type: 'get-status', url });
 }
 
@@ -54,12 +27,10 @@ async function getStatusFromService(url) {
 async function updateAllLinksOnPage(documentUrl, root = document) {
   const links = root.querySelectorAll('a');
 
-  console.debug('found ', links.length, 'links');
-  for (let i = 0; i < links.length; i++) {
-    const link = links[i];
-    if (isNormalMarkableLink(link, documentUrl)) {
-      // eslint-disable-next-line no-await-in-loop
-      const status = await getStatusFromService(link.href);
+  console.debug('content: found ', links.length, 'links');
+  await Promise.all([...links].map(async (link) => {
+    if (isNormalLink(link, documentUrl)) {
+      const status = await getStatusFromServiceWorker(link.href);
 
       link.classList.remove('marked-as-done');
       link.classList.remove('marked-as-todo');
@@ -69,67 +40,55 @@ async function updateAllLinksOnPage(documentUrl, root = document) {
         link.classList.add(`marked-as-${status}`);
       }
     }
-  }
+  }));
 }
 
 /**
- *
+ * Only mark normal links and ignore links to anchors on the same page.
  * @param linkElement {HTMLAnchorElement}
- * @param documentUrl {string}
+ * @param documentUrl {string} Based on the current domain additing checks are done.
  * @return {boolean}
  */
-function isNormalMarkableLink(linkElement, documentUrl) {
+// eslint-disable-next-line no-unused-vars
+function isNormalLink(linkElement, documentUrl) {
   const url = linkElement.href;
   if (url === '') {
     return false;
   }
 
   // A plain '#' is often used for buttons and menubars. Can be ignored.
-  if (linkElement.getAttribute('href') === '#') {
-    return false;
-  }
-
-  if (url === documentUrl) {
-    return true;
-  }
-
-  // ignore header links in the sidebar
-  if (documentUrl.startsWith('https://experienceleague.adobe.com/')
-    && linkElement.matches('#container [data-id="toc"] a[href^="#"]')) {
-    return false;
-  }
-
-  const isSamePage = prepareUrl(url) === prepareUrl(documentUrl);
-  if (isSamePage) {
-    return true;
-  }
-
-  return true;
+  return linkElement.getAttribute('href') !== '#';
 }
 
-// TODO: eliminate this duplicate function
-function prepareUrl(url) {
-  try {
-    const urlObject = new URL(url);
-    // In general, hash are ignored.
+function watchPageForDynamicallyAddedLinks() {
+  // TODO: this needs to be more generic. We need to watch for changes in the DOM.
+  // Maybe use a MutationObserver?
 
-    // Search must be respected for confluence-wiki. (/pages/viewpage.action?pageId=123)
-    // but on other pages the "?lang=en" should be ignored.
+  // TODO: prevent multiple observers. Currently this function is called multiple times.
 
-    let filteredSearch = urlObject.search.replace(/lang=.*$/, '');
+  // Some pages load content later. Need to add a trigger to process the links later.
+  if (window.location.href.startsWith('https://learning.oreilly.com/')) {
+    // Button to show the toc
+    document.querySelectorAll('a.sbo-toc-thumb').forEach((a) => {
+      a.addEventListener('click', () => updateAllLinksOnPage(window.location.href));
+    });
+  }
 
-    // In confluence-wiki, there is a suffix when clicking the sidebar which should be ignored.
-    // https://wiki.corp.example.com/display/ABC/Link?src=contextnavpagetreemode
-    filteredSearch = urlObject.search.replace(/src=contextnavpagetreemode/, '');
-
-    if (filteredSearch === '?') {
-      filteredSearch = '';
-    }
-
-    return urlObject.origin + urlObject.pathname + filteredSearch;
-  } catch (error) {
-    console.error(`Can not parse as url=${url}, error=${error}`);
-    return null;
+  if (window.location.href.startsWith('https://wiki.corp')) {
+    // eslint-disable-next-line no-unused-vars
+    const mutationObserver = new MutationObserver((mutationList, observer) => {
+      // Use traditional 'for loops' for IE 11
+      for (const mutation of mutationList) {
+        if (mutation.type === 'childList') {
+          console.debug('Wiki: Sidebar was loaded.');
+          updateAllLinksOnPage(window.location.href);
+        }
+      }
+    });
+    mutationObserver.observe(
+      document.querySelector('div.plugin_pagetree_children'),
+      { childList: true, subtree: false },
+    );
   }
 }
 
